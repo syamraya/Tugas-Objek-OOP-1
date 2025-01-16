94b0ce62417e656ca69340ef499a3bd9860a3aa4
syamraya <syamrayaibrahimahmad@gmail.com>
2025-01-16 14:10:27 +0700

    commit

--- Laptop.java
+++ Laptop.java
@@ -26,15 +26,19 @@ public class Laptop {
             case 1 :
             System.out.println("Spesifikasi: ");
             System.out.println(specs[choice - 1]);
+            break;
             case 2 :
             System.out.println("Spesifikasi: ");
             System.out.println(specs[choice - 1]);
+            break;
             case 3 :
             System.out.println("Spesifikasi: ");
             System.out.println(specs[choice - 1]);
+            break;
             case 4 :
             System.out.println("Spesifikasi: ");
             System.out.println(specs[choice - 1]);
+            break;
             case 5 :
             System.out.println("Spesifikasi: ");
             System.out.println(specs[choice - 1]);
